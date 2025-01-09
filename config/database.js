66db258358db03d5724771a08076ad8a6308e5c9
Rey <rey.valdes@alumnos.ucn.cl>
2025-01-09 19:57:08 -0300

    feat: using postgresql en vezc de mysql

--- config/database.js
+++ config/database.js
@@ -8,20 +8,29 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    port: process.env.DB_PORT || 5432, // Puerto predeterminado de PostgreSQL
+    dialect: "postgres", // Cambiado de "mysql" a "postgres"
   },
   test: {
     username: "root",
     password: null,
     database: "parkingdb",
     host: "127.0.0.1",
-    dialect: "mysql",
+    port: 5432,
+    dialect: "postgres", // Cambiado de "mysql" a "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres", // Cambiado de "mysql" a "postgres"
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Si usas un proveedor que requiere SSL, como Heroku
+      },
+    },
   },
 };
